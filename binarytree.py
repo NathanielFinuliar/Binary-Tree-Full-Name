0a4b5c13e24f81cacb1c61024930c97629b73343
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-04 21:28:11 +0800

    Added add_child function inside BinaryTreeNode class

--- binarytree.py
+++ binarytree.py
@@ -5,3 +5,8 @@ class BinaryTreeNode():
         self.left = None
         self.right = None
         self.data = data
+
+    # Add child to the parent class
+    def add_child(self, data):
+        if(data == self.data):
+            return
